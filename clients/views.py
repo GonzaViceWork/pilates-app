2fdea5632e66bc91fddb2a7f05006b0de1b58905
GonzaViceWork <gonzavice.work@gmail.com>
2025-01-15 17:27:38 -0300

    APIs de Clientes y Sessiones con Django REST Framework

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Client, Session
+from .serializers import ClientSerializer, SessionSerializer
 
 # Create your views here.
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+class SessionViewSet(viewsets.ModelViewSet):
+    queryset = Session.objects.all()
+    serializer_class = SessionSerializer
